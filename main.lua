8a35ad813badb394137a08096e0438f5f750ac2b
Ben Parrish <bparrish99@gmail.com>
2021-12-11 10:02:36 -0500

    Initial checkin

---
+++ main.lua
@@ -0,0 +1,90 @@
+require 'objects'
+
+logs = {}
+
+function log(s)
+	table.insert(logs, s)
+end
+
+function love.load()
+log('loading')
+	love.window.setMode(1500,1200)
+	love.window.setTitle('Fuck Tom Nelson!  No, sorry, that was mean.')
+	gamestate = { scale = 1, range = 30000, ringSpacing = 5000 }
+	objects = {}
+
+	gamestate.windowWidth = love.graphics.getWidth()
+	gamestate.windowHeight = love.graphics.getHeight()
+	gamestate.windowOriginX = -(gamestate.windowWidth / 2)
+	gamestate.windowOriginY = -(gamestate.windowHeight / 2)
+
+	image = love.graphics.newImage( "ship.png" )
+	objects.myShip = Movable:new(0, 0, image, 190, 10)
+	myShip = objects.myShip -- convenience
+
+	love.graphics.setBackgroundColor(.3,.3,.5);
+
+end
+
+function love.draw()
+
+	local maxDimension = math.max(gamestate.windowWidth, gamestate.windowHeight)
+
+	-- draw range rings
+	love.graphics.setColor(.9, .7, .8)
+	for ringDistance = gamestate.ringSpacing, gamestate.range, gamestate.ringSpacing do
+		local radius = ringDistance * maxDimension / gamestate.range / 2
+		love.graphics.print(ringDistance, myShip:windowPositionX() - 20, myShip:windowPositionY() - radius - 20)
+		love.graphics.circle('line', myShip:windowPositionX(), myShip:windowPositionY(), radius)
+	end
+
+	if love.mouse.isDown(1) then
+		x, y = love.mouse.getPosition()
+		love.graphics.setColor(.9, .2, .2)
+		love.graphics.line(x, y, myShip:windowPositionX(), myShip:windowPositionY())
+	end
+
+	love.graphics.setColor(1,1,1);
+	for k,v in pairs(objects) do
+		v:draw()
+	end
+
+	love.graphics.print("Range: " .. gamestate.range, 1400, 1180)
+
+	love.graphics.setColor(.5, .5, 1);
+	for i,v in ipairs(logs) do
+		love.graphics.print(v, gamestate.windowWidth - 200, i * 20)
+	end
+
+end
+
+function love.update( dt )
+
+	if love.mouse.isDown(1) then
+		x, y = love.mouse.getPosition()
+		myShip:setDirection(math.deg(math.atan2(myShip:windowPositionY() - y, myShip:windowPositionX() - x)) - 90)
+		local dx = myShip:windowPositionX() - x
+		local dy = myShip:windowPositionY() - y
+		myShip:setSpeed(math.sqrt ( dx * dx + dy * dy ))
+	end
+	
+	for k,v in pairs(objects) do
+		if (v.move) then
+			v:move(dt)
+		end
+	end
+end
+
+function love.mousepressed(x, y, i)
+	if (i == 2) then
+		logs = {}
+	end
+end
+
+function love.wheelmoved(x, y)
+	log("Wheel moved: " .. y)
+	if (y > 0 and gamestate.range > 1000) or (y < 0 and gamestate.range < 100000) then
+		gamestate.range = gamestate.range - (y * 1000)
+	end
+end
+
---
+++ objects.lua
@@ -0,0 +1,53 @@
+ObjectInSpace = {}
+
+function ObjectInSpace:new(x, y, imageFile)
+    o = {}
+    o.x = x or 0
+    o.y = y or 0
+    o.imageFile = imageFile
+    setmetatable(o, self)
+    self.__index = self
+    return o
+end
+
+function ObjectInSpace:draw()
+    love.graphics.draw(self.imageFile, self:windowPositionX(), self:windowPositionY(), 0, gamestate.scale, gamestate.scale, self.imageFile:getWidth() / 2, self.imageFile:getHeight() / 2);
+end
+
+function ObjectInSpace:windowPositionX()
+    return gamestate.windowOriginX + gamestate.windowWidth + self.x
+end
+
+function ObjectInSpace:windowPositionY()
+    return gamestate.windowOriginY + gamestate.windowHeight + self.y 
+end
+
+
+
+Movable = ObjectInSpace:new()
+
+function Movable:new(x, y, imageFile, dir, speed)
+    o = ObjectInSpace:new(x, y, imageFile)
+    o.dir = dir or 0
+    o.speed = speed or 0
+    self.__index = self
+    setmetatable(o, self)
+    return o
+end
+
+function Movable:draw()
+    love.graphics.draw(self.imageFile, self:windowPositionX(), self:windowPositionY(), math.rad(self.dir), gamestate.scale, gamestate.scale, self.imageFile:getWidth() / 2, self.imageFile:getHeight() / 2);
+end
+
+function Movable:setDirection(d)
+    self.dir = d
+end
+
+function Movable:setSpeed(s)
+    self.speed = s
+end
+
+function Movable:move(dt)
+    self.x = self.x + (math.sin(math.rad(self.dir)) * self.speed * dt)
+    self.y = self.y - (math.cos(math.rad(self.dir)) * self.speed * dt)
+end
