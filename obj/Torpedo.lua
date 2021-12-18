83683567d6a1188d684b6e23ef67a25931db26f4
Ben Parrish <bparrish99@gmail.com>
2021-12-18 16:59:36 -0500

    gotta hit empty shield to die

--- obj/Torpedo.lua
+++ obj/Torpedo.lua
@@ -43,7 +43,6 @@ function Torpedo:checkCollision(o)
             local shieldHit = getShieldSide(o.dir, hitDir)
             local hitStrength = math.random(25) + 25
             log("Shield " .. shieldHit .. " HIT from angle " .. hitDir .. " for " .. hitStrength);
-            o.shields[shieldHit] = o.shields[shieldHit] - hitStrength;
             remove(self)
             if (o.shields[shieldHit] <= 0) then
                 table.insert(objects, Explosion:new(o.x, o.y))
@@ -55,6 +54,7 @@ function Torpedo:checkCollision(o)
                 littleExplosion.seconds = .5
                 table.insert(objects, littleExplosion)
             end
+            o.shields[shieldHit] = o.shields[shieldHit] - hitStrength;
         end
     end
 end
